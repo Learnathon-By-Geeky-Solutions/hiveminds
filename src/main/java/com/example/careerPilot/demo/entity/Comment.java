d0fd464d765d02d9b63ab61cc84b683f3da9a007
kaizen2112 <tasrif701@gmail.com>
2025-03-13 20:04:27 +0600

    structured it as it wasnt working

--- src/main/java/com/example/careerPilot/demo/entity/Comment.java
+++ src/main/java/com/example/careerPilot/demo/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.example.careerPilot.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,4 +16,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "Comment")
 public class Comment {
 
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
