91984475705c53616530c7d2d5125fc8ccaf2f65
Efty34 <efty17.hasan@gmail.com>
2025-03-16 15:24:29 +0600

    UserDetails card added

--- CareerPilot_Client/src/App.jsx
+++ CareerPilot_Client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Indexpage from "./pages/Indexpage";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
-import Footer from "./components/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -16,7 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- CareerPilot_Client/src/pages/Profile.jsx
+++ CareerPilot_Client/src/pages/Profile.jsx
@@ -1,32 +1,24 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import AuthService from "../services/AuthService";
-import { Button } from "@/components/ui/button";
+import UserDetails from "@/components/profile/UserDetails";
 
 const Profile = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      await AuthService.logout();
-      navigate("/login");
-    } catch (error) {
-      console.error("Logout failed", error);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await AuthService.logout();
+  //     navigate("/login");
+  //   } catch (error) {
+  //     console.error("Logout failed", error);
+  //   }
+  // };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-800">
-      <div className="bg-blue-950 p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-4">Welcome to Home</h2>
-        <button
-          onClick={handleLogout}
-          className="bg-red-500 text-white px-4 py-2 rounded w-full"
-        >
-          Logout
-        </button>
-        <Button>Shadcn</Button>
-      </div>
+    <div className="min-h-screen bg-background">
+      <main className="container py-24 px-4 sm:px-6">
+        <div className="flex flex-col lg:flex-row gap-8">
+          <UserDetails />
+        </div>
+      </main>
     </div>
   );
 };
