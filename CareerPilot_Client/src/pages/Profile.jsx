00a10d4248f0f14a82442ec9ee1b4c3721601a10
Efty34 <efty17.hasan@gmail.com>
2025-03-16 14:50:04 +0600

    Shadcn Added

--- CareerPilot_Client/src/pages/Profile.jsx
+++ CareerPilot_Client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import AuthService from "../services/AuthService";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -24,6 +25,7 @@ const Profile = () => {
         >
           Logout
         </button>
+        <Button>Shadcn</Button>
       </div>
     </div>
   );
--- src/main/java/com/example/careerPilot/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/careerPilot/demo/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5176/")
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
