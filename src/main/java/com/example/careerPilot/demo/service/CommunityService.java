5f22a28850dcf22edaa0d591ee66b09ae50af3bc
kaizen2112 <tasrif701@gmail.com>
2025-03-21 20:30:45 +0600

    community get bug resolved

--- src/main/java/com/example/careerPilot/demo/service/CommunityService.java
+++ src/main/java/com/example/careerPilot/demo/service/CommunityService.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +37,8 @@ public class CommunityService {
         return communityRepository.findAll(pageable).map(this::mapToDTO);
     }
 
-    public Community createCommunity(@RequestBody CommunityDTO communityDTO, @RequestBody Long userId) {
+    @Transactional
+    public Community createCommunity(CommunityDTO communityDTO, Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         Community community = new Community();
         community.setName(communityDTO.getName());
@@ -46,13 +49,21 @@ public class CommunityService {
         if (communityDTO.getVisibility() != null) {
             community.setVisibility(Community.Visibility.valueOf(communityDTO.getVisibility().toUpperCase()));
         }
+
+        // Save the Community first
+        Community savedCommunity = communityRepository.save(community);
+
+
         CommunityUser communityUser = new CommunityUser();
         communityUser.setCommunity(community);
         communityUser.setUser(user);
         communityUser.setRole(CommunityUser.role.ADMIN);
-        Community community1 =  communityRepository.save(community);
-        communityUserRepository.save(communityUser);
-        return community1;
+        communityUser.setStatus(CommunityUser.status.ACCEPTED);
+        communityUser.setJoinDate(LocalDateTime.now());
+
+//        Community community1 =  communityRepository.save(community);
+//        communityUserRepository.save(communityUser);
+        return savedCommunity;
 
     }
 
